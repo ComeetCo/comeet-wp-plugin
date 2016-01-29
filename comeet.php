d9c364770c1b74c21d4b7c1bf3ab4d1387497380
Asana <manishs@enwrap.io>
2016-01-29 07:09:10 +0530

    Text rephrasing and field positioning

--- comeet.php
+++ comeet.php
@@ -70,7 +70,7 @@ if(!class_exists('Comeet')) {
         'advanced_search' => 1,
 		'comeet_color' => '278fe6',
 		'comeet_bgcolor' => '',
-		'comeet_stylesheet' => 'comeet-basic.css'
+		'comeet_stylesheet' => 'comeet-default.css'
       );
 
       $saved = get_option($this->db_opt);
@@ -135,7 +135,7 @@ if(!class_exists('Comeet')) {
 
       add_settings_section(
         'comeet_other_settings',
-        'Careers Website Homepage',
+        'Careers Website',
         array($this, 'other_text'),
         'comeet'
       );
@@ -156,6 +156,13 @@ if(!class_exists('Comeet')) {
         'comeet_other_settings'
       );
       add_settings_field(
+        'comeet_stylesheet',
+        'Stylesheet',
+        array($this, 'comeet_stylesheet_input'),
+        'comeet',
+        'comeet_other_settings'
+      );
+      add_settings_field(
         'comeet_color',
         'Form Main Color',
         array($this, 'comeet_color_input'),
@@ -169,13 +176,6 @@ if(!class_exists('Comeet')) {
         'comeet',
         'comeet_other_settings'
       );	  
-      add_settings_field(
-        'comeet_stylesheet',
-        'Stylesheet',
-        array($this, 'comeet_stylesheet_input'),
-        'comeet',
-        'comeet_other_settings'
-      );
       add_settings_section(
         'comeet_other_blank',
         '',
@@ -241,9 +241,9 @@ if(!class_exists('Comeet')) {
       $options = $this->get_options();
       echo '<div>';
 	  echo '<select name="'.$this->db_opt.'[comeet_stylesheet]" id="comeet_stylesheet" style="width:200px">';
-	  echo '<option value="comeet-basic.css"'.($options['comeet_stylesheet'] == 'comeet-basic.css' ? ' selected="selected"' : '').'>Basic</option>';
-	  echo '<option value="comeet-cards.css"'.($options['comeet_stylesheet'] == 'comeet-cards.css' ? ' selected="selected"' : '').'>Cards</option>';
-	  echo '<option value="comeet-two-columns.css"'.($options['comeet_stylesheet'] == 'comeet-two-columns.css' ? ' selected="selected"' : '').'>Two columns</option>';
+	  echo '<option value="comeet-default.css"'.($options['comeet_stylesheet'] == 'comeet-default.css' ? ' selected="selected"' : '').'>Style 1</option>';
+	  echo '<option value="comeet-style2.css"'.($options['comeet_stylesheet'] == 'comeet-style2.css' ? ' selected="selected"' : '').'>Style 2</option>';
+	  echo '<option value="comeet-style3.css"'.($options['comeet_stylesheet'] == 'comeet-style3.css' ? ' selected="selected"' : '').'>Style 3</option>';
 	  echo '</select></div>';
 	}
     /**
