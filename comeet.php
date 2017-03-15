b89b019455e60fccc95c4eaa32744c59843c88f2
Tomer Dikerman <tomidik@gmail.com>
2017-03-15 10:19:04 +0200

    update version to 1.2.1

--- comeet.php
+++ comeet.php
@@ -3,7 +3,7 @@
  * Plugin Name: Comeet
  * Plugin URI: http://www.comeet.co
  * Description: Job listing page using the Comeet API.
- * Version: 1.2
+ * Version: 1.2.1
  * Author: Comeet
  * Author URI: http://www.comeet.co
  * License: Apache 2
