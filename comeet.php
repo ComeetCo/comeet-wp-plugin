66c5a9146d973a94bc76654999c16a4a14ee18b3
Yonatan Ran <yonatan.ran@gmail.com>
2018-05-30 08:28:56 -0400

    Version Bump Changelog update

--- comeet.php
+++ comeet.php
@@ -3,7 +3,7 @@
  * Plugin Name: Comeet
  * Plugin URI: http://support.comeet.co/knowledgebase/wordpress-plug-in/
  * Description: Job listing page using the Comeet API.
- * Version: 2.0
+ * Version: 2.0.0.1
  * Author: Comeet
  * Author URI: http://www.comeet.co
  * License: Apache 2
@@ -54,7 +54,7 @@ if (!class_exists('Comeet')) {
 
     class Comeet {
         //current plugin version - used to display version as a comment on comeet pages and in the settings page
-        public $version = '2.0';
+        public $version = '2.0.0.1';
         var $plugin_url;
         var $plugin_dir;
         //All commet options are stored in the wp options table in an array
