f74a1aff940db4c1dfebd1a321cd36c648575d38
Michael Wender <github@michaelwender.com>
2025-06-05 09:17:27 -0400

    Updating "All Jobs" permalink
    Previously the "All Jobs" permalink was being built via `<?php echo site_url() . '/' . $post->post_name; ?>`. By appending the `post_name` to the `site_url`, we are just adding the page's slug. This means we're generating the wrong URL for sub-pages. To always get the full URL to the page, we must use `get_permalink()` which is the preferred WordPress way for retrieving a page's URL.

--- templates/comeet-position-page.php
+++ templates/comeet-position-page.php
@@ -1,7 +1,6 @@
 <?php if ($show_all_link) : ?>
-	<?php $post = get_post(get_the_ID()); ?>
 	<div class="all-jobs-link">
-		<a href="<?php echo site_url() . '/' . $post->post_name; ?>">&larr; All Jobs</a>
+		<a href="<?php echo get_permalink( get_the_ID() ) ?>">&larr; All Jobs</a>
 	</div>
 <?php endif; ?>
 <?php include 'comeet-position-page-common.php' ?>
